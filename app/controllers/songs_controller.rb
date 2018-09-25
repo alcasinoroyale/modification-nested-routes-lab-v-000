4c953b0867253638e277d86849af3fa0362b9b44
Alex Zdatny <alexzdatny@gmail.com>
2018-09-25 21:29:20 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,7 @@ class SongsController < ApplicationController
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
         redirect_to artists_path, alert: "Artist not found."
+      else
     @song = Song.find(params[:id])
   end
 
